6ec642bd2ece924f9352b9001f99f4cb0a1a9a85
Brian Chung <brianchung23@gmail.com>
2014-06-18 21:23:55 -0700

    Convert to backbone.js

--- js/index.js
+++ js/index.js
@@ -4,85 +4,131 @@ var div = '#result';
 var mean_div = '#mean';
 var median_div = "#median";
 
-$(function() {
-	$('#add_class').on('click', function() {
-		chrome.tabs.create({url: "options.html"});
-	});
 
-	// load the dropdown with saved classes
-	storageArea.get(null, function(data) {
-		var $dropdown = $('<select>');
+(function($) {
 
-		$dropdown.append($('<option>').attr({value: ''}).text('-- Select Class --'));
+	var Grades = Backbone.Model.extend({
+		defaults: {
+			overall: '----',
+			mean   : '----',
+			median : '----',
+			url    : null
+		},
 
-		for(var key in data) {
-			var $option = $('<option>');
-			$option.attr({
-				value: key
-			}).text(key);
+		getGrades: function(name) {
+			var self = this;
+			storageArea.get(name, function(items) {
+				var url = items[name][0];
+				var secret_number = items[name][1];
+				self.set('url', url);
 
-			$dropdown.append($option);
-		}
-
-		$('#classes').append($dropdown);
-
-		// load page with selected class
-		$dropdown.on('change', function() {
-			var _class = $(this).val();
-			loadPage(_class);
-		});
-	});
-});
-
-// given class name, load the percentages and such.
-function loadPage(name) {
-	storageArea.get(name, function(items) {
-		var url = items[name][0];
-		var secret_number = items[name][1];
-
-		getOverall(url, secret_number, div, function(grade) {
-			var $overall = $('#overall');
-			$overall.text(grade);
-
-			// get the mean to compare with overall
-			jQuery.get(url + PAGES.stats, 
-				function(data) {
-					var $data = $(data);
-					var $rows = $data.find('tr');
-
-					$.each($rows, function(indx, val) {
-						var $col1 = $(val).children().first();
-						var target = $col1.text();
-
-						if('Mean:' == target) {
-							var mean = $(val).children().last().text();
-
-							if(parseFloat(grade.split('%')[0]) > parseFloat(mean)) {
-								$overall.removeClass("bad");
-								$overall.addClass("good");
-							} else {
-								$overall.removeClass("good");
-								$overall.addClass("bad");
-							}
-						}
+				getOverall(url, secret_number, div, function(grade) {
+					self.set('overall', grade);
+				});
+				
+				getMeanMedian(url, mean_div, median_div,
+					function(mean) {
+						self.set('mean', mean);
+					}, function(median) {
+						self.set('median', median);
 					});
-				}
-			);
+			});
+		}
+	});
+
+	var GradeView = Backbone.View.extend({
+		id: 'grades',
+
+		initialize: function() {
+			_.bindAll(this, 'render', 'setColors');
+
+			this.model = new Grades();
+			this.model.bind('change', this.render); // on any change, rerender view 
+
+			$('#add_class').on('click', function() {
+				chrome.tabs.create({url: "options.html"});
+			});
+
+			var self = this;
+
+			// load the dropdown with saved classes
+			storageArea.get(null, function(data) {
+				var $dropdown = $('<select>');
+
+				$dropdown.append($('<option>').attr({value: ''}).text('-- Select Class --'));
+
+				_.each(data, function(value, key, list) {
+					var $option = $('<option>');
+					$option.attr({
+						value: key
+					}).text(key);
+
+					$dropdown.append($option);
+				});
+
+				$('#grades #classes').append($dropdown);
+
+				// load page with selected class
+				$dropdown.on('change', function() {
+					var _class = $(this).val();
+
+					self.model.getGrades(_class);
+				});
+			});
+
+			this.render();
+		},
+
+		render: function() {
+			var grade  = this.model.get('overall');
+			var mean   = this.model.get('mean');
+			var median = this.model.get('median');
+			var url    = this.model.get('url');
+
+			var $overall = $('#overall');
+			$overall.text(grade + "%");
+			$('#mean_number').text(mean + "%");
+			$('#median_number').text(median + "%");
+
+			this.setColors();
 
 			// open course standings on click
 			$overall.off();
 			$overall.on('click', function() {
 				chrome.tabs.create({url: url + PAGES.standings});
 			});
-		});
-		
-		getMeanMedian(url, mean_div, median_div, 
-			function(mean) {
-				$('#mean_number').text(mean + "%");
+		},
 
-			}, function(median) {
-				$('#median_number').text(median + "%");
-			});
+		setColors: function() {
+			var grade  = this.model.get('overall');
+			var mean   = this.model.get('mean');
+			var median = this.model.get('median');
+			var url    = this.model.get('url');
+			var $overall = $('#overall');
+
+			if(parseFloat(grade.split('%')[0]) > parseFloat(mean)) {
+				$overall.removeClass("bad");
+				$overall.addClass("good");
+			} else {
+				$overall.removeClass("good");
+				$overall.addClass("bad");
+			}
+		}
 	});
+
+	var gradeView = new GradeView();
+
+})(jQuery);
+
+
+
+
+$(function() {
+
+});
+
+// given class name, load the percentages and such.
+function loadPage(name) {
+	
 }
 
